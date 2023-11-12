f384bce4575759e074446bb03734846088ee2db1
ivr88 <114075652+ivr88@users.noreply.github.com>
2023-11-12 20:09:21 +0300

    Initial Commit

---
+++ UnitConversions/UnitConversionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionsApp.swift
+//  UnitConversions
+//
+//  Created by Вадим Исламов on 12.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
